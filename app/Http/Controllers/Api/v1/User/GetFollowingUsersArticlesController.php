19c09e1d5b7c32feb47dbe14d5ebd729b376e5ec
justice chimobi <chimobi.justice@gmail.com>
2024-10-11 15:29:35 +0100

    Fix: error on returning followed articles by authors

--- app/Http/Controllers/Api/v1/User/GetFollowingUsersArticlesController.php
+++ app/Http/Controllers/Api/v1/User/GetFollowingUsersArticlesController.php
@@ -4,43 +4,36 @@ namespace App\Http\Controllers\Api\v1\User;
 
 use App\Models\Article;
 use App\Helpers\ResponseHelper;
-use App\Http\Resources\Article\ArticleResource;
+use App\Http\Resources\Article\FollowingAuthorArticlesResource;
 use App\Http\Resources\PaginationResource;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class GetFollowingUsersArticlesController extends Controller
 {
-    public function getFollowingUsersArticles(Request $request) {
-        $user = auth()->user();
-    
-        if (!$user) {
-            return ResponseHelper::error("Unauthorized", 401);
-        }
-    
-        $limit = (int) $request->query("limit", 10);
-
-        $followingUserIds = $user->followings()->pluck('user_id');
-
+    public function getFollowingUsersArticles(Request $request){
         try {
-            $articles = Article::whereIn('user_id', $followingUserIds)
-                ->withCount(['articleComments', 'articleLikes'])
-                ->orderByRaw('(article_comments_count + article_likes_count) DESC')
-                ->paginate($limit);
-    
+            $user = auth()->user();
+            $perPage = (int) $request->query("limit", 25);
+
+            $articles = Article::whereHas('user.followers', function ($query) use ($user) {
+                $query->where('follower_id', $user->id);
+            })->withCount('articleComments')
+                ->orderBy('article_comments_count', 'desc')
+                ->paginate($perPage);
+
             return ResponseHelper::success(
-                message: "Success",
+                message: "success",
                 data: [
-                    "articles" => ArticleResource::collection($articles),
+                    "articles" => FollowingAuthorArticlesResource::collection($articles),
                     "pagination" => PaginationResource::make($articles)
                 ]
             );
         } catch (\Exception $e) {
             return ResponseHelper::error(
-                message: "An error occurred while fetching articles", 
+                message: "An error occurred while fetching articles",
                 statusCode: 500
             );
         }
     }
-    
 }
